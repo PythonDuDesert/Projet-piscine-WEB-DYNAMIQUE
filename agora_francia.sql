9597e86c7bd9d594fb53078e87a579ed308f3d6e
Olivier YAMMINE <olivier.yammine@gmail.com>
2025-05-27 17:17:50 +0200

    Update agora_francia.sql
    fix

--- agora_francia.sql
+++ agora_francia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 27 mai 2025 à 13:12
+-- Généré le : mar. 27 mai 2025 à 15:17
 -- Version du serveur : 9.1.0
 -- Version de PHP : 8.3.14
 
@@ -111,7 +111,7 @@ CREATE TABLE IF NOT EXISTS `articles` (
   `Image` varchar(255) NOT NULL,
   `IDAcheteurVendeur` int NOT NULL COMMENT 'Clé étrangère',
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=38 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `articles`
@@ -128,7 +128,7 @@ INSERT INTO `articles` (`ID`, `NomArticle`, `Categorie`, `DateAjout`, `PrixAchat
 (8, 'Massue \"Travail n°9\"', 'Rare', '0386-12-29', 59, 45, 29, '2025-05-26 19:35:19', 'Massue utilisée contre l\'Hydre de Lerne', 1, 0, 'images/articles/Massue.jpg', 2),
 (9, 'Flèches empoisonnées ', 'Commun', '0386-12-18', 34, 29, 19, '2025-05-26 19:38:28', 'Pack de 6 flèches pour recréer le combat contre les oiseaux du lac Stymphale.', 20, 12, 'images/articles/Fleches.jpg', 2),
 (10, 'Collier à Trois Têtes \"Gardien des Enfers\"', 'Commun', '0396-05-14', 39, 25, 30, '2025-06-04 21:42:39', 'Collier ajustable pour chien (ou humain audacieux)\r\nMatériaux : Nylon résistant + détails glow-in-the-dark\r\nTaille : Adaptable (40-60 cm)', 15, 4, 'images/articles/Collier.jpg', 3),
-(11, 'Os à Mâcher', 'Commun', '0396-05-05', 12, 8, 10, '2025-05-26 19:45:36', 'Os en caoutchouc ultra-résistant, infusé au \"jus du fleuve Styx\" (arôme faux bacon)\r\nAvantages :\r\nDurée de vie : \"Éternelle\" (en théorie)\r\nNettoie les dents et distrait votre molosse pendant des heures\r\nFlotte dans l’eau (testé dans le Styx, résultats variables)', 30, 18, 'images/articles/Os.jpg', 3),
+(11, 'Os à Mâcher', 'Commun', '0396-05-05', 12, 8, 10, '2025-05-26 19:45:36', 'Os en caoutchouc ultra-résistant, infusé au \"jus du fleuve Styx\" (arôme faux bacon)\r\nAvantages :\r\nDurée de vie : \"Éternelle\" (en théorie)\r\nNettoie les dents et distrait votre molosse pendant des heures\r\nFlotte dans l’eau (testé dans le Styx, résultats var', 30, 18, 'images/articles/Os.jpg', 3),
 (12, 'La Toison d\'Or', 'Premium', '0396-05-18', 2499, 1999, 2100, '2025-06-10 21:47:53', 'Légendaire Toison d\'Or, symbole de pouvoir et de prospérité.', 1, 0, 'images/articles/Toison.jpg', 3),
 (13, 'Le Trône d\'Hadès', 'Premium', '0336-06-06', 14, 9, 14, '2025-06-03 21:55:53', '\"Le véritable trône du Dieu des Enfers, récupéré après négociation avec Perséphone.\r\n\r\nMatière : Obsidienne pure incrustée d\'os de Titans\r\n\r\nDimensions : 2,5m de haut x 1,8m de large (poids : 888 kg)\r\n\r\nFonctionnalités :\r\n\r\nSiège chauffant à lave (réglabl', 1, 0, 'images/articles/Trone.jpeg', 4),
 (14, 'Cruche', 'Commun', '0379-03-29', 40, 25, 30, '2025-06-04 16:15:00', 'Cruche en terre cuite de Haute Egypte.', 18, 14, 'images/articles/cruche.jpg', 10),
@@ -142,7 +142,19 @@ INSERT INTO `articles` (`ID`, `NomArticle`, `Categorie`, `DateAjout`, `PrixAchat
 (22, 'Vase egyptien en terre cuite avec ances', 'Commun', '0390-05-06', 45, 60, 39, '2025-06-07 18:30:00', 'Vase egyptien en terre cuite clair avec ances pour le tenir.', 12, 11, 'images/articles/vase1.jpg', 8),
 (23, 'Vase egyptien en terre cuite avec ances et motifs', 'Commun', '0390-08-02', 45, 29, 40, '2025-06-07 19:00:00', 'Vase egyptien en terre cuite sombre avec ances et motifs egyptien gravés dessus.', 10, 12, 'images/articles/vase2.jpg', 9),
 (24, 'Vase egyptien avec  motifs peints', 'Commun', '0390-11-30', 40, 25, 30, '2025-06-07 12:30:00', 'Vase egyptien avec  motifs peints.', 17, 21, 'images/articles/vase3.jpg', 6),
-(25, 'Cube ancien mystique', 'Premium', '0390-08-15', 1500, 1300, 1430, '2025-06-07 13:10:00', 'Cube ancien mystique d\'origine inconnue.', 1, 0, 'images/articles/allspark.jpg', 10);
+(25, 'Cube ancien mystique', 'Premium', '0390-08-15', 1500, 1300, 1430, '2025-06-07 13:10:00', 'Cube ancien mystique d\'origine inconnue.', 1, 0, 'images/articles/allspark.jpg', 10),
+(26, 'Agenda Runique', 'Rare', '1002-07-28', 100, 80, 75, '2025-06-01 08:20:27', 'Organiseur en cuir gravé de runes. Livré avec un caillou pour graver les tâches.', 4, 1, 'images/articles/agendarunique.jpg', 12),
+(27, 'Bouclier mural', 'Commun', '1021-09-21', 80, 68, 65, '2025-05-27 08:27:51', 'Bouclier mural décoratif datant des véritables Vikings. \r\nParfait en cas de manifestation.', 10, 3, 'images/articles/boucliermural.jpg', 13),
+(28, 'Carte marine du Vinland', 'Commun', '1002-08-23', 30, 27, 25, '2025-05-31 08:31:53', 'Reproduction (plus ou moins fidèle) d’une carte utilisée pour atteindre les terres inconnues de l’ouest. Légende incluse : \"Attention aux serpents de mer\".', 15, 6, 'images/articles/cartemarine.jpg', 11),
+(29, 'Casque Viking', 'Commun', '1018-01-03', 20, 18, 16, '2025-06-14 08:34:25', 'Casque Viking à cornes. Parfait en temps de guerre ou pour l\'anniversaire de Titouan.', 30, 5, 'images/articles/casque-viking.jpg', 14),
+(30, 'Habits de Viking', 'Premium', '1002-05-01', 340, 290, 200, '2025-02-20 08:37:17', 'Vêtements de viking originel. D\'après la légende, le roi Sven l\'aurait porté.', 1, 0, 'images/articles/deguisementviking.jpg', 11),
+(31, 'Drakkar Miniature', 'Rare', '1021-10-07', 90, 85, 80, '2025-05-07 08:40:01', 'Representation fidele et miniature du bateau des vikings.\r\nDimension 37.7cm x 12.1cm', 14, 2, 'images/articles/drakkar.jpg', 13),
+(32, 'Drakkar Geant', 'Premium', '1017-11-30', 2500, 2250, 2200, '2025-05-27 08:42:36', 'Véritable drakkar de l\'époque des vikings restauré à neuf.\r\nParfait pour partir en vacances en famille ou bien aller faire la guerre.\r\nDimension : 30m x 13m  ', 1, 0, 'images/articles/drakkarGeant.jpg', 14),
+(33, 'Duo déguisement vikings', 'Commun', '1001-12-05', 70, 64, 60, '2025-06-24 08:45:12', 'Duo de déguisement vikings (1 homme, 1 femme)\r\nD\'après la légende vous seriez ridicule peu importe votre charisme en portant ces déguisements.\r\nLes haches, le bouclier, les casques sont bien entendu pas livrés avec le déguisement.', 4, 1, 'images/articles/duo-deguisement-viking.jpg', 12),
+(34, 'Epée d\'Askeladd', 'Premium', '1010-10-13', 400, 350, 330, '2025-05-27 08:50:05', 'Forgée dans les forges brumeuses des côtes galloises et trempée dans les mensonges d’un roi.\r\nCette arme aurait été utilisée dans plus de 36 duels malheureusement aucun témoin n\'a survécu.', 1, 0, 'images/articles/epee.askeladd.jpg', 12),
+(35, 'Haches miniatures de décoration', 'Commun', '1002-07-12', 53, 46, 40, '2025-06-10 08:54:25', 'Petites haches en bois gravées, à accrocher dans la maison ou à offrir pour prouver son courage.', 12, 3, 'images/articles/hachesmini.jpg', 11),
+(36, 'Les 10 grands secrets d\'un manipulateur', 'Commun', '1003-03-02', 24, 22, 17, '2025-06-02 08:57:10', 'Dans ce livre, Askeladd retrace les 10 plus grands secrets qui lui ont permis de conquérir le monde.\r\nEdition - Hachette ', 21, 8, 'images/articles/livre.askeladd.jpg', 11),
+(37, 'Parfum \"Essence de conquête\"', 'Rare', '1021-07-17', 180, 150, 120, '2025-05-31 09:01:07', 'Une mixture à base de pin nordique, de sueur de berserker et d’ambre. \"Pour imposer le respect sans lever la hache\"', 6, 1, 'images/articles/parfum.jpg', 13);
 
 -- --------------------------------------------------------
 
