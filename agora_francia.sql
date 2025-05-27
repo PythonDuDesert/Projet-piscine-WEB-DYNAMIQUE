274b63e8431678e400fa3fb8eff324e1a717186b
Olivier YAMMINE <olivier.yammine@gmail.com>
2025-05-27 13:58:06 +0200

    Update agora_francia.sql

--- agora_francia.sql
+++ agora_francia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 27 mai 2025 à 09:18
+-- Généré le : mar. 27 mai 2025 à 11:57
 -- Version du serveur : 9.1.0
 -- Version de PHP : 8.3.14
 
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS `acheteurs_vendeurs` (
   `TypeCarte` tinyint NOT NULL,
   `NumeroCarte` varchar(255) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `acheteurs_vendeurs`
@@ -61,11 +61,7 @@ INSERT INTO `acheteurs_vendeurs` (`ID`, `Nom`, `Prenom`, `DateNaissance`, `Email
 (7, 'Isis', 'GrandeMagicienne', '0295-07-21', 'isis@agorafrancia.fr', '0654321987', 69000, '31 Palais de l’Est', 'isisis400', 'EpouseDeOsiris', 'images/Isis.png', '0400-05-27', 2510, 3, '3456 7890 1234 5678'),
 (8, 'Horus', 'FauconCeleste', '0305-11-11', 'horus@agorafrancia.fr', '0678901234', 31000, '40 Ciel d’Héliopolis', 'horusse400', 'FanDeFaucon', 'images/Horus.png', '0400-05-27', 4000, 1, '4567 8901 2345 6789'),
 (9, 'Seth', 'SeigneurDuDesert', '0280-09-09', 'seth@agorafrancia.fr', '0611122233', 60000, '55 Désert Rouge', 'sept400', 'sept777', 'images/Seth.png', '0400-05-27', 515, 2, '5678 9012 3456 7890'),
-(10, 'Re', 'SoleilEternel', '0275-12-25', 're@agorafrancia.fr', '0699998888', 33000, '68 Bateau Solaire', 'reflexion400', 'SuperSoleil', 'images/Re.png', '0400-05-27', 4785, 3, '6789 0123 4567 8901'),
-(11, 'Erikson', 'Leif', '0970-08-13', 'leif.erikson@gmail.com', '0722345487', 36110, '1 Rue de l\'expedition', 'Lexplorateur', 'Islande12', 'images/leif.jpg', '1002-04-27', 3254, 2, '4322 3245 4356 1254'),
-(12, 'Lucius Artorius Castus ', 'Askeladd', '0969-10-30', 'askeladd@agorafrancia.fr', '4567984190', 9988, '4 Rue du Jutland du Nord', 'Askeladd', 'Pirate3', 'images/askeladd.png', '1001-07-21', 12000, 3, '9171 9452 6821 1780 '),
-(13, 'Jarl', 'Ulf', '0976-03-05', 'ulf.leouf@gmail.com', '4587432192', 9990, '343 Rue du Vinland', 'LeNavigateur', 'ulfff', 'images/ulf.png', '1021-09-19', 8467, 1, '6305 1571 8000 5331'),
-(14, 'Knut', 'Knutsson', '0985-12-01', 'knut.legrand@agorafrancia.fr', '0612566360 ', 29000, '45 Avenue du Jelling ', 'KnutLeGrand', 'TheGreat', 'images/knut.jpg', '1017-09-23', 6000, 2, '5279 9917 4335 2424');
+(10, 'Re', 'SoleilEternel', '0275-12-25', 're@agorafrancia.fr', '0699998888', 33000, '68 Bateau Solaire', 'reflexion400', 'SuperSoleil', 'images/Re.png', '0400-05-27', 4785, 3, '6789 0123 4567 8901');
 
 -- --------------------------------------------------------
 
@@ -115,7 +111,7 @@ CREATE TABLE IF NOT EXISTS `articles` (
   `Image` varchar(255) NOT NULL,
   `IDAcheteurVendeur` int NOT NULL COMMENT 'Clé étrangère',
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `articles`
@@ -135,19 +131,17 @@ INSERT INTO `articles` (`ID`, `NomArticle`, `Categorie`, `DateAjout`, `PrixAchat
 (11, 'Os à Mâcher', 'Commun', '0396-05-05', 12, 8, 10, '2025-05-26 19:45:36', 'Os en caoutchouc ultra-résistant, infusé au \"jus du fleuve Styx\" (arôme faux bacon)\r\nAvantages :\r\nDurée de vie : \"Éternelle\" (en théorie)\r\nNettoie les dents et distrait votre molosse pendant des heures\r\nFlotte dans l’eau (testé dans le Styx, résultats var', 30, 18, 'images/articles/Os.jpg', 3),
 (12, 'La Toison d\'Or', 'premium', '0396-05-18', 2499, 1999, 2100, '2025-06-10 21:47:53', 'Légendaire Toison d\'Or, symbole de pouvoir et de prospérité.', 1, 0, 'images/articles/Toison.jpg', 3),
 (13, 'Le Trône d\'Hadès', 'premium', '0336-06-06', 14, 9, 14, '2025-06-03 21:55:53', '\"Le véritable trône du Dieu des Enfers, récupéré après négociation avec Perséphone.\r\n\r\nMatière : Obsidienne pure incrustée d\'os de Titans\r\n\r\nDimensions : 2,5m de haut x 1,8m de large (poids : 888 kg)\r\n\r\nFonctionnalités :\r\n\r\nSiège chauffant à lave (réglabl', 1, 0, 'images/articles/Trone.jpeg', 4),
-(14, 'Le Trône d\'Hadès', 'premium', '0336-06-06', 14, 9, 14, '2025-06-03 21:55:53', '\"Le véritable trône du Dieu des Enfers, récupéré après négociation avec Perséphone.\r\n\r\nMatière : Obsidienne pure incrustée d\'os de Titans\r\n\r\nDimensions : 2,5m de haut x 1,8m de large (poids : 888 kg)\r\n\r\nFonctionnalités :\r\n\r\nSiège chauffant à lave (réglabl', 1, 0, 'images/articles/Trone.jpeg', 4),
-(15, 'Agenda Runique', 'Rare', '1002-07-28', 100, 80, 75, '2025-06-01 08:20:27', 'Organiseur en cuir gravé de runes. Livré avec un caillou pour graver les tâches.', 4, 1, 'images/articles/agendarunique.jpg', 12),
-(16, 'Bouclier mural', 'Commun', '1021-09-21', 80, 68, 65, '2025-05-27 08:27:51', 'Bouclier mural décoratif datant des véritables Vikings. \r\nParfait en cas de manifestation.', 10, 3, 'images/articles/boucliermural.jpg', 13),
-(17, 'Carte marine du Vinland', 'Commun', '1002-08-23', 30, 27, 25, '2025-05-31 08:31:53', 'Reproduction (plus ou moins fidèle) d’une carte utilisée pour atteindre les terres inconnues de l’ouest. Légende incluse : \"Attention aux serpents de mer\".', 15, 6, 'images/articles/cartemarine.jpg', 11),
-(18, 'Casque Viking', 'Commun', '1018-01-03', 20, 18, 16, '2025-06-14 08:34:25', 'Casque Viking à cornes. Parfait en temps de guerre ou pour l\'anniversaire de Titouan.', 30, 5, 'images/articles/casque-viking.jpg', 14),
-(19, 'Deguisement Viking', 'premium', '1002-05-01', 340, 290, 200, '2025-02-20 08:37:17', 'Deguisement de viking originel. D\'après la légende, le roi Sven l\'aurait porté.', 1, 0, 'images/articles/deguisementviking.jpg', 11),
-(20, 'Drakkar Miniature', 'Rare', '1021-10-07', 90, 85, 80, '2025-05-07 08:40:01', 'Representation fidele et miniature du bateau des vikings.\r\nDimension 37.7cm x 12.1cm', 14, 2, 'images/articles/drakkar.jpg', 13),
-(21, 'Drakkar Geant', 'premium', '1017-11-30', 2500, 2250, 2200, '2025-05-27 08:42:36', 'Véritable drakkar de l\'époque des vikings restauré à neuf.\r\nParfait pour partir en vacances en famille ou bien aller faire la guerre.\r\nDimension : 30m x 13m  ', 1, 0, 'images/articles/drakkarGeant.jpg', 14),
-(22, 'Duo déguisement vikings', 'Commun', '1001-12-05', 70, 64, 60, '2025-06-24 08:45:12', 'Duo de déguisement vikings (1 homme, 1 femme)\r\nD\'après la légende vous seriez ridicule peu importe votre charisme en portant ces déguisements.\r\nLes haches, le bouclier, les casques sont bien entendu pas livrés avec le déguisement.', 4, 1, 'images/articles/duo-deguisement-viking.jpg', 12),
-(23, 'Epée d\'Askeladd', 'premium', '1010-10-13', 400, 350, 330, '2025-05-27 08:50:05', 'Forgée dans les forges brumeuses des côtes galloises et trempée dans les mensonges d’un roi.\r\nCette arme aurait été utilisée dans plus de 36 duels malheureusement aucun témoin n\'a survécu.', 1, 0, 'images/articles/epee.askeladd.jpg', 12),
-(24, 'Haches miniatures de décoration', 'Commun', '1002-07-12', 53, 46, 40, '2025-06-10 08:54:25', 'Petites haches en bois gravées, à accrocher dans la maison ou à offrir pour prouver son courage.', 12, 3, 'images/articles/hachesmini.jpg', 11),
-(25, 'Les 10 grands secrets d\'un manipulateur', 'Commun', '1003-03-02', 24, 22, 17, '2025-06-02 08:57:10', 'Dans ce livre, Askeladd retrace les 10 plus grands secrets qui lui ont permis de conquérir le monde.\r\nEdition - Hachette ', 21, 8, 'images/articles/livre.askeladd.jpg', 11),
-(26, 'Parfum \"Essence de conquête\"', 'Rare', '1021-07-17', 180, 150, 120, '2025-05-31 09:01:07', 'Une mixture à base de pin nordique, de sueur de berserker et d’ambre. \"Pour imposer le respect sans lever la hache\"', 6, 1, 'images/articles/parfum.jpg', 13);
+(14, 'Cruche', 'Commun', '0379-03-29', 40, 25, 30, '2025-06-04 16:15:00', 'Cruche en terre cuite de Haute Egypte.', 18, 14, 'images/articles/cruche.jpg', 10),
+(15, 'Lampe à huile', 'Commun', '0379-08-15', 20, 12, 15, '2025-06-05 12:10:00', 'Lampe à huile en terre cuite Egyptienne. Légèrement usée, bon état.', 10, 6, 'images/articles/lampe_huile.jpg', 6),
+(16, 'Livre ancien Oui Ou Nan?', 'Premium', '0380-09-02', 1120, 1049, 1089, '2025-06-12 17:15:00', 'Livre ancien authentique du Oui Ou Nan? par Anis CHAARI.', 1, 0, 'images/articles/livre_OuiOuNan.png', 7),
+(17, 'Livre sur l\'art d\'aller aux toilettes avant les cours.', 'Premium', '0382-01-21', 899, 820, 869, '2025-06-05 08:30:00', 'Livre mythique sur l\'art d\'aller aux toilettes avant les cours. Par Nicolas COUCHOUD.', 1, 0, 'images/articles/livre_Precautions.png', 7),
+(18, 'Livre/Guide pour ne pas utiliser son téléphone en cours.', 'Premium', '0382-02-05', 910, 835, 869, '2025-06-05 10:45:00', 'Livre/Guide légendaire pour ne pas utiliser son téléphone en cours. Par Noreddine EUTAMENE.', 1, 0, 'images/articles/livre_telephone.png', 7),
+(19, 'Statue d\'Anubis', 'Rare', '0380-12-01', 359, 305, 330, '2025-06-01 21:00:00', 'Statue authentique du dieu Anubis.', 6, 10, 'images/articles/Statue_Anubis.jpg', 5),
+(20, 'Statuette de chat noir egyptien', 'Rare', '0389-03-29', 310, 255, 289, '2025-06-06 16:15:00', 'Statuette de chat noir egyptien avec dorures.', 5, 8, 'images/articles/statuette_chat.png', 5),
+(21, 'Stele de Re', 'Premium', '0390-03-29', 899, 800, 849, '2025-06-14 15:30:00', 'Stele authentique du dieu Re.', 1, 1, 'images/articles/Stele_Ra.jpg', 10),
+(22, 'Vase egyptien en terre cuite avec ances', 'Commun', '0390-05-06', 45, 60, 39, '2025-06-07 18:30:00', 'Vase egyptien en terre cuite clair avec ances pour le tenir.', 12, 11, 'images/articles/vase1.jpg', 8),
+(23, 'Vase egyptien en terre cuite avec ances et motifs', 'Commun', '0390-08-02', 45, 29, 40, '2025-06-07 19:00:00', 'Vase egyptien en terre cuite sombre avec ances et motifs egyptien gravés dessus.', 10, 12, 'images/articles/vase2.jpg', 9),
+(24, 'Vase egyptien avec  motifs peints', 'Commun', '0390-11-30', 40, 25, 30, '2025-06-07 12:30:00', 'Vase egyptien avec  motifs peints.', 17, 21, 'images/articles/vase3.jpg', 6);
 
 -- --------------------------------------------------------
 
